8edc0c3adb4e231c767518541b1bd7bea0c9b575
Christopher T.A <82207467+tototheshadow@users.noreply.github.com>
2022-05-28 12:32:02 +0700

    Add files via upload

---
+++ predict sales/app.py
@@ -0,0 +1,48 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import json
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    print(final_features)
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Sales should be $ {}'.format(output))
+
+@app.route('/tes',methods=['POST'])
+def tes():
+
+    array = []
+    for x in request.form.values():
+        array.append(x)
+    arr = np.array(array)
+
+    hasil = json.dumps(array)
+    
+
+    return render_template('index.html', prediction_text='Hasilnya {}'.format(arr))
+
+@app.route('/results',methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
